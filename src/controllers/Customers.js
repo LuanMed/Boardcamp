8a7c82d4bfe80991c80c674d2345019e361b94ec
LuanMed <luanmede@gmail.com>
2023-02-08 13:57:03 -0300

    fix: bug in update customer

--- src/controllers/Customers.js
+++ src/controllers/Customers.js
@@ -45,7 +45,7 @@ export async function updateCustomer (req, res) {
         const idExist = await db.query(`SELECT * FROM customers WHERE id = ${id}`);
         if(idExist.rows.length === 0) return res.status(404).send("ID não existe");
 
-        const cpfExist = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf}'`);
+        const cpfExist = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf} and id <> ${id}'`);
         if (cpfExist.rows.length !== 0) return res.status(409).send("Esse CPF já foi cadastrado");
 
         await db.query(`UPDATE customers SET name='${name}', phone=${phone}, cpf=${cpf}, birthday='${birthday}' WHERE id = ${id}`);
