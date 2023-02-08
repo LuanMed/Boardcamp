6d712a2804c3c69d204eaa1ab3234e94b16e0a77
LuanMed <luanmede@gmail.com>
2023-02-08 14:26:42 -0300

    fix: bug when inserting a game

--- src/controllers/Games.js
+++ src/controllers/Games.js
@@ -16,10 +16,10 @@ export async function insertGames (req, res) {
     if (stockTotal <= 0 || pricePerDay <= 0) return res.status(400).send("Valor deve ser maior que 0");
 
     try {
-        const nameExist = await db.query(`SELECT * FROM customers WHERE name = '${name}'`);
+        const nameExist = await db.query(`SELECT * FROM games WHERE name = '${name}'`);
         if (nameExist.rows.length !== 0) return res.status(409).send("Já existe um jogo com esse nome");
-
-        await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ('${name}', ${image}, ${stockTotal}, '${pricePerDay}');`);
+        console.log(nameExist.rows)
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ('${name}', '${image}', ${stockTotal}, ${pricePerDay})`);
         
         res.sendStatus(201);
     } catch (error) {
