95d6369ca487a96e79c22f4f952489606b720d36
LuanMed <luanmede@gmail.com>
2023-02-09 13:16:59 -0300

    fix: post and get rentals

--- src/controllers/Rentals.js
+++ src/controllers/Rentals.js
@@ -1,9 +1,36 @@
 import { db } from "../config/database.connection.js";
 import dayjs from "dayjs";
 
-export async function listRentals (req, res) {
+export async function listRentals(req, res) {
     try {
-        const rentals = await db.query("SELECT * FROM rentals");
+        const rentals = await db.query(`SELECT * FROM rentals;`);
+
+        //const customers = await db.query(`SELECT rentals.*, customers.name, customers.id FROM rentals RIGHT JOIN customers ON rentals."customerId" = customers.id`);
+        //const games = await db.query(`SELECT rentals.*, games.name, games.id FROM rentals RIGHT JOIN customers ON rentals."gameId" = game.id`);
+        //console.log(customers.rows)
+        // const rentals = await db.query(`SELECT * FROM rentals;`);
+        // const listRentals = rentals.rows.map()
+
+        const customers = [];
+        const games = [];
+        for (let i = 0; i < rentals.rows.length; i++) {
+            customers.push((await db.query(`SELECT customers.id, customers.name FROM customers WHERE id = $1`, [rentals.rows[i].customerId])).rows[0]);
+            games.push((await db.query(`SELECT games.id, games.name FROM games WHERE id = $1`, [rentals.rows[i].gameId])).rows[0]);
+        }
+        console.log(customers);
+        console.log(games);
+        //const game = (await db.query(`SELECT games.id, games.name FROM games WHERE id = $1`, [rentals.rows[0].gameId])).rows[0];
+        // console.log(game)
+
+        for (let i = 0; i < rentals.rows.length; i++) {
+            rentals.rows[i].customer = customers[i];
+            rentals.rows[i].game = games[i];
+        }
+
+        const returnDate = dayjs();
+        const aux = returnDate - rentals.rows[0].rentDate;
+        console.log(aux)
+        console.log(returnDate.$d)
 
         res.send(rentals.rows);
     } catch (error) {
@@ -11,8 +38,9 @@ export async function listRentals (req, res) {
     }
 }
 
-export async function insertRentals (req, res) {
+export async function insertRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body;
+
 
     if (daysRented <= 0) return res.status(400).send("Valor deve ser maior que 0");
 
@@ -20,16 +48,28 @@ export async function insertRentals (req, res) {
 
         const customerExist = await db.query(`SELECT * FROM customers WHERE id = $1`, [customerId]);
         if (customerExist.rows.length === 0) return res.status(400).send("Cliente não foi encontrado");
+
         const gameExist = await db.query('SELECT * FROM games WHERE id = $1', [gameId]);
         if (gameExist.rows.length === 0) return res.status(400).send("Jogo não foi encontrado");
-        
+
+        const rentalsForThisGame = await db.query(`SELECT * FROM rentals WHERE "gameId" = $1`, [gameExist.rows[0].id]);
+        if (rentalsForThisGame.rows.length >= gameExist.rows[0].stockTotal) return res.status(400).send("Jogo está em falta no estoque");
+
         const rentDate = dayjs();
         const originalPrice = daysRented * gameExist.rows[0].pricePerDay;
 
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice") VALUES ($1, $2, $3, $4, $5);`, [customerId, gameId, daysRented, rentDate, originalPrice]);
-        
+
         res.sendStatus(201);
     } catch (error) {
         res.status(500).send(error.message);
     }
+}
+
+export async function returnRental(req, res) {
+    const { id } = req.params;
+    const body = req.body;
+
+    const returnDate = dayjs();
+    console.log(returnDate)
 }
